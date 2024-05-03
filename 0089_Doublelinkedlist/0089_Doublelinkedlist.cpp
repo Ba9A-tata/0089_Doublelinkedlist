e6dd5390b0787fab46f0803b53597ae847b0c466
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-03 22:22:45 +0700

    membuat else

--- 0089_Doublelinkedlist/0089_Doublelinkedlist.cpp
+++ 0089_Doublelinkedlist/0089_Doublelinkedlist.cpp
@@ -12,5 +12,32 @@ Node *START = NULL;
 
 void addNode()
 {
+	Node *newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; // Assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // Assign value to the data field od the new node
 
+	// Insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{ // step 2: insert the new node at the beginning
+
+	if (START != NULL && newNode->noMhs == START->noMhs)
+	{
+		cout << "\033[31Duplicate roll numbers not allowed\033[0m" << endl;
+		return;
+	}
+	// if the list is empty, amke the new node the START
+	newNode->next = START; // step 3: make the new node point to the first node
+	if (START != NULL)
+	{
+		START->prev = newNode; // step 4: make the first node point to the new node
+	}
+	newNode->prev = NULL; // step5: make the new node point to NULL
+	START = newNode;		// step 6: make the new node the fisrt node
+	}
+	else
+	{
+
+	}
 }
