a91b5bac179bb946a37a1afc545430c5f53b9cf5
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-03 23:26:15 +0700

    memabuat void search data

--- 0089_Doublelinkedlist/0089_Doublelinkedlist.cpp
+++ 0089_Doublelinkedlist/0089_Doublelinkedlist.cpp
@@ -142,13 +142,31 @@ void traverse()
 		Node *currentNode = START; // step 1
 		while (currentNode != NULL) // step 2
 		{
-			cout << currentNode->noMhs << " " << currentNode->name << endl;
-			currentNode = currentNode->next;
+			cout << currentNode->noMhs << " " << currentNode->name << endl; // step 3
+			currentNode = currentNode->next;								// step 4
 		}
 	}
 }
 
 void revtraverse()
+{
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "\nRecord in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+void searchData()
 {
 
 }
